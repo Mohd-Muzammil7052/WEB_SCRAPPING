36a5811e7f329ff159bc0b2feb877804068fa715
Mohd-Muzammil7052 <muzammilaib306.in>
2024-04-23 23:40:48 +0530

    first commit

--- application.py
+++ application.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, request,jsonify
 from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
+import logging
 from urllib.request import urlopen as uReq
 import pymongo
+from pymongo.mongo_client import MongoClient
 
 application = Flask(__name__) # initializing a flask app
 app=application
@@ -24,15 +26,14 @@ def index():
             flipkartPage = uClient.read()
             uClient.close()
             flipkart_html = bs(flipkartPage, "html.parser")
-            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
+            bigboxes = flipkart_html.findAll("div", {"class": "cPHDOP col-12-12"})
             del bigboxes[0:3]
             box = bigboxes[0]
             productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
             prodRes = requests.get(productLink)
             prodRes.encoding='utf-8'
             prod_html = bs(prodRes.text, "html.parser")
-            print(prod_html)
-            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
+            commentboxes = prod_html.find_all('div', {'class': "RcXBOT"})
 
             filename = searchString + ".csv"
             fw = open(filename, "w")
@@ -42,41 +43,50 @@ def index():
             for commentbox in commentboxes:
                 try:
                     #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
-
+                    name = commentbox.div.div.find_all('p', {'class': '_2NsDsF AwS1CA'})[0].text
+                    
                 except:
                     name = 'No Name'
 
                 try:
-                    #rating.encode(encoding='utf-8')
+                    # rating.encode(encoding='utf-8')
                     rating = commentbox.div.div.div.div.text
-
 
                 except:
                     rating = 'No Rating'
 
                 try:
-                    #commentHead.encode(encoding='utf-8')
+                    # commentHead.encode(encoding='utf-8')
                     commentHead = commentbox.div.div.div.p.text
-
+                    
                 except:
                     commentHead = 'No Comment Heading'
+
                 try:
                     comtag = commentbox.div.div.find_all('div', {'class': ''})
                     #custComment.encode(encoding='utf-8')
                     custComment = comtag[0].div.text
+                    
                 except Exception as e:
                     print("Exception while creating dictionary: ",e)
 
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
                 reviews.append(mydict)
-            client = pymongo.MongoClient("mongodb+srv://mohdmuzammil:muzammil@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
+            # logging.info("log my final result {}".format(reviews))
+
+            # connecting with MONGODB
+            uri = "mongodb+srv://mohdmuzammil:muzammil@cluster0.vfigipw.mongodb.net/?retryWrites=true&w=majority"
+            # Create a new client and connect to the server
+            client = MongoClient(uri)
+
             db = client['review_scrap']
             review_col = db['review_scrap_data']
             review_col.insert_many(reviews)
+            
             return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
+            
             print('The Exception message is: ',e)
             return 'something is wrong'
     # return render_template('results.html')
